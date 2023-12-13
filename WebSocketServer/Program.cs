d5e65617c81fa4da2cf8ad0c545901904dadcc33
Arup Dutta <arupdutta@microsoft.com>
2023-12-12 16:03:26 -0800

    Simplify the websocket server code

--- WebSocketServer/Program.cs
+++ WebSocketServer/Program.cs
@@ -2,13 +2,9 @@ using System.Net.WebSockets;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
-
-// setup web socket for stream in
 app.UseWebSockets();
 app.Use(async (context, next) =>
 {
@@ -19,7 +15,6 @@ app.Use(async (context, next) =>
             using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
             try
             {
-                string partialData = "";
                 while (webSocket.State == WebSocketState.Open || webSocket.State == WebSocketState.CloseSent)
                 {
                     byte[] receiveBuffer = new byte[2048];
@@ -29,38 +24,14 @@ app.Use(async (context, next) =>
                     if (receiveResult.MessageType != WebSocketMessageType.Close)
                     {
                         string data = Encoding.UTF8.GetString(receiveBuffer).TrimEnd('\0');
-
-                        try
-                        {
-                            if (receiveResult.EndOfMessage)
-                            {
-                                data = partialData + data;
-                                partialData = "";
-
-                                if (data != null)
-                                {
-
-                                    Console.OutputEncoding = System.Text.Encoding.Unicode;
-                                    Console.WriteLine($"\n [{DateTime.UtcNow}] {data}");
-                                }
-                            }
-                            else
-                            {
-                                partialData = partialData + data;
-                            }
-                        }
-                        catch (Exception ex)
-                        { Console.WriteLine($"Exception 1 -> {ex}"); }
+                        Console.OutputEncoding = System.Text.Encoding.Unicode;
+                        Console.WriteLine($"\n [{DateTime.UtcNow}] {data}");
                     }
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Exception 2 -> {ex}");
-            }
-            finally
-            {
-
             }
         }
         else
@@ -75,7 +46,6 @@ app.Use(async (context, next) =>
 });
 
 app.UseAuthorization();
-
 app.MapControllers();
 app.UseHttpsRedirection();
 app.Run();
